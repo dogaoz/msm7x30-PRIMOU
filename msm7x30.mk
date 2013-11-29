7c9b47ae346ad0f569df4cb21d7a4a5b65d38a34
doga.ozkaraca <doga.ozkaraca@gmail.com>
2013-11-29 16:09:43 +0200

    Kernel-Headers and includes update.

--- msm7x30.mk
+++ msm7x30.mk
@@ -14,6 +14,7 @@
 # limitations under the License.
 #
 
+
 # Permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/handheld_core_hardware.xml:system/etc/permissions/handheld_core_hardware.xml \
